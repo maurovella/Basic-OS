91471f72b295965424495d7c68f59b8d282eda86
maurov <mvella@itba.edu.ar>
2023-04-12 00:52:30 -0300

    Creation of vista

--- src/vista.c
+++ src/vista.c
@@ -1,24 +1,52 @@
-#include <stdio.h>
-#include <string.h>
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+#include "include/manager.h"
+
+/* TEST --------*/
 
 int main(int argc, char * argv[])
 {
-    char arg1[256], arg2[256], arg3[256];
-    int arg_length[3];
+    char shm_name[256] = {0}, reading_sem_name[256] = {0}, closing_sem_name[256] = {0};
+    
+    fgets(shm_name, 256, stdin);
+    fgets(reading_sem_name, 256, stdin);
+    fgets(closing_sem_name, 256, stdin);
 
-    fgets(arg1, 256, stdin);
-    fgets(arg2, 256, stdin);
-    fgets(arg3, 256, stdin);
+    shm_name[strcspn(shm_name, "\n")] = '\0'; // Elimina el carácter de nueva línea de shm_name
+    reading_sem_name[strcspn(reading_sem_name, "\n")] = '\0'; // Elimina el carácter de nueva línea de sem_name
+    closing_sem_name[strcspn(closing_sem_name, "\n")] = '\0';
 
-    arg_length[0] = strlen(arg1);
-    arg_length[1] = strlen(arg2);
-    arg_length[2] = strlen(arg3);
+    shm_info shm;
+    sem_info reading_sem;
+    sem_info closing_sem;
+    
+    shm.name = shm_name;
+    reading_sem.name = reading_sem_name;
+    closing_sem.name = closing_sem_name;
 
-    arg1[arg_length[0] - 1] = 0;
-    arg2[arg_length[1] - 1] = 0;
-    arg3[arg_length[2] - 1] = 0;
-    printf("%s\n", arg1);
-    printf("%s\n", arg2);
-    printf("%s\n", arg3);
+    open_shm(&shm);
+    open_sem(&reading_sem);
+    open_sem(&closing_sem);
+
+    wait_sem(&closing_sem);
+    md5_info md5;
+
+    int finished = 0;
+    for (int i = 0; !finished; i++) {
+        wait_sem(&reading_sem);
+        pread(shm.fd, &md5, sizeof(md5_info), i * sizeof(md5_info));
+        printf("file_name: %s -- hash: %s -- pid: %d\n", md5.file_name, md5.hash, md5.pid);
+
+        if (md5.files_left <= 1) {
+            finished = 1;
+        }
+    }
+    post_sem(&closing_sem);
+    close_shm(&shm);
+    close_sem(&reading_sem);
+    close_sem(&closing_sem);
+
     return 0;
 }
+
+/* -------- TEST */
