82efaedb09c1b07c9066a306d86b9cd3290b0d86
maurov <mvella@itba.edu.ar>
2023-04-12 00:50:55 -0300

    shm size tweaks in struct

--- src/utils/shm_utils.c
+++ src/utils/shm_utils.c
@@ -1,18 +1,22 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 #include "./../include/manager.h"
 #include <sys/mman.h>
 
+#define SHM_SIZE 16834
+
 void create_shm (shm_info * shm) {
-    if ((shm->fd = shm_open(shm->name, O_RDWR|O_CREAT, S_IRUSR|S_IWUSR)) == -1) {
+    if ((shm->fd = shm_open(shm->name, O_CREAT|O_RDWR, S_IRUSR|S_IWUSR)) == -1) {
         perror("Error creating shared memory.");
         exit(ERR_CREATING_SHM);
     }
 
-    if (ftruncate(shm->fd, shm->size) == -1) {
+    if (ftruncate(shm->fd, SHM_SIZE) == -1) {
         perror("Error truncating shared memory.");
         exit(ERR_TRUNCATING_SHM);
     }
 
-    if ((shm->addr = mmap(NULL, shm->size, PROT_READ|PROT_WRITE, MAP_SHARED, shm->fd, 0)) == MAP_FAILED) {
+    if ((shm->addr = mmap(NULL, SHM_SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, shm->fd, 0)) == MAP_FAILED) {
         perror("Error mapping shared memory.");
         exit(ERR_MAPPING_SHM);
     }
@@ -24,7 +28,7 @@ void open_shm (shm_info * shm) {
         exit(ERR_OPENING_SHM);
     }
 
-    if ((shm->addr = mmap(NULL, shm->size, PROT_READ, MAP_SHARED, shm->fd, 0)) == MAP_FAILED) {
+    if ((shm->addr = mmap(NULL, SHM_SIZE, PROT_READ, MAP_SHARED, shm->fd, 0)) == MAP_FAILED) {
         perror("Error mapping shared memory.");
         exit(ERR_MAPPING_SHM);
     }
@@ -38,7 +42,7 @@ void write_shm (int fd, void * buf, size_t length, int pos) {
 }
 
 void close_shm (shm_info * shm) {
-    if (munmap(shm->addr, shm->size) == -1) {
+    if (munmap(shm->addr, SHM_SIZE) == -1) {
         perror("Error unmapping shared memory.");
         exit(ERR_UNMAPPING_SHM);
     }
